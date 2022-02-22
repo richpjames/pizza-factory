648a56c2d351df67aa6287fbe76249f3b0825d51
richpjames <rich@Richards-MacBook-Pro.local>
2022-02-22 15:11:22 +0000

    add active state to app component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,25 @@ import { Sauce } from "./components/Sauce";
 import { Topping } from "./components/Topping";
 import { Cooking } from "./components/Cooking";
 
+type ComponentNames = "dough" | "sauce";
+
 function App() {
+  const [activeComponent, setActiveComponent] =
+    useState<ComponentNames>("dough");
   return (
     <div className="App">
-      <Dough active onDone={() => {}} onError={() => {}} />
-      <Sauce />
+      <Dough
+        active={activeComponent === "dough"}
+        onDone={() => {
+          setActiveComponent("sauce");
+        }}
+        onError={() => {}}
+      />
+      <Sauce
+        active={activeComponent === "sauce"}
+        onDone={() => {}}
+        onError={() => {}}
+      />
       <Topping />
       <Cooking />
     </div>
